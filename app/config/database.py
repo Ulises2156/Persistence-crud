935464cf03842ed0279ba723c3b109a75f367f5f
Ulises2156 <100093761+Ulises2156@users.noreply.github.com>
2024-02-05 20:23:25 -0300

    Update database.py

--- app/config/database.py
+++ app/config/database.py
@@ -1,17 +1,18 @@
 import csv
+from app.models.product import Product
 
 class Database:
     def __init__(self, filename = "product_data.csv"):
         self.filename = filename
     
-    def save_data(self, data):
+    def save_data(self, products):
         with open(self.filename, "w", newline="") as csvfile:
             fieldnames = ["id","name","description", "price", "stock", "category"]
             writer = csv.DictWriter(csvfile, fieldnames= fieldnames)
 
             writer.writeheader()
-            for product in data:
-                writer.writerow(product.__dict__)
+            for product in products:
+                writer.writerow({field: getattr(product, field) for field in fieldnames})
     
     def load_data(self):
         data = []
@@ -19,8 +20,18 @@ class Database:
             with open(self.filename, "r", newline= "") as csvfile:
                 reader = csv.DictReader(csvfile)
                 for row in reader:
-                    data.append(row)
+                    product_data ={
+                        "product_id": row["id"],
+                        "name": row["name"],
+                        "description": row["description"],
+                        "price": row["price"],
+                        "stock": row["stock"],
+                        "category": row["category"],
+                    }
+                    product =Product(**product_data)
+                    data.append(product)
         except FileNotFoundError:
             pass
         return data
+    
     
