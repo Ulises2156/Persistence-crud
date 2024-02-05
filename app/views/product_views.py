c4fcc017cbbba94fe5f807c9ae6621df782b6ece
Ulises2156 <100093761+Ulises2156@users.noreply.github.com>
2024-02-05 20:25:44 -0300

    Update product_views.py

--- app/views/product_views.py
+++ app/views/product_views.py
@@ -1,21 +1,42 @@
 class ProductViews:
-    def display_menu(self):
+    def display_menu(self, exit_message=None):
         print("\n--- Menú Product ---")
         print("1. Create Product")
         print("2. Read Product")
         print("3. Update Product")
         print("4. Delete Product")
         print("5. Exit")
+        if exit_message:
+            print(exit_message)
+
         return int(input("Enter you choice: "))
-    
 
+    def get_products_data(self, existing_ids):
+        while True:
+            try:
+                product_id = int(input("Enter Product ID: "))
+                if product_id in existing_ids:
+                    print("Product with this ID already exits. Please choose another ID.")
+                else:
+                    break
+            except ValueError: # estamos probrando un try y except, porque sigue saliendo error en id
+                print("Invalid input. Please enter a valid numeric ID.")
 
-    def get_products_data(self):
-        product_id = int(input("Enter Product ID: "))
         name = input ("Enter Product Name: ")
         description = input("Enter Product Description: ")
-        price = float(input("Enter Product Price: "))
-        stock = int(input("Enter Product Stock: "))
+
+        while True:
+            try:
+                price = float(input("Enter Product Price: "))
+                break
+            except ValueError:
+                print("Invalid input. Please enter a valid numeric price")
+        while True:
+            try:
+                stock = int(input("Enter Product Stock: "))
+                break
+            except ValueError:
+                print("Invalid input. Please enter a valid numeric stock")
         category = input("Enter Product Category: ")
         return {"product_id": product_id, 
                 "name": name, 
@@ -36,10 +57,18 @@ class ProductViews:
         print("\n Enter new for the product (press Enter to kepp the current value):")
         name = input(f"Current Name: {product.name}, Enter New Name: ") or product.name
         description = input(f"Current Description: {product.description}, Enter New Description: ") or product.description
-        price = float(input(f"Current Price: {product.price}, Enter New Price: ")) or product.price
-        stock = int(input(f"Current Stock: {product.stock}, Enter New Stock: ")) or product. stock
+        price = input(f"Current Price: {product.price}, Enter New Price: ") or product.price
+        stock = input(f"Current Stock: {product.stock}, Enter New Stock: ") or product. stock
         category = input(f"Current Category: {product.category}, Enter New Category: ") or product.category
-        return{"name": name, "description": description, "price": price, "stock": stock, "category": category}
+        
+        update_data = {
+            "name": name,
+            "description": description,
+            "price": price,
+            "stock": stock,
+            "category": category
+        }
+        return update_data
     
     def display_message(self, message):
         return message
