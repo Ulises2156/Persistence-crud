8b15fdab3500638cb9b3e5217453bfabf4abcf3e
Ulises2156 <100093761+Ulises2156@users.noreply.github.com>
2024-02-13 17:20:55 -0300

    Update product_controller.py

--- app/controllers/product_controller.py
+++ app/controllers/product_controller.py
@@ -1,76 +1,41 @@
 from app.models.product import Product
-from app.views.product_views import ProductViews
-from app.config.database import Database
 
 class ProductController:
-    def __init__(self):
-        self.product_views = ProductViews()
-        self.database = Database()
+    def __init__(self, database):
+        self.database = database
         self.products = self.database.load_data()
-    
-    def create_products(self):
-        existing_ids = [product.id for product in self.products]
-        product_data = self.product_views.get_products_data(existing_ids)
-        new_products = Product(**product_data)
-        self.products.append(new_products)
-        self.database.save_data(self.products)
-        self.product_views.display_message("Product created successfully.")
 
+    def create_product(self, product_data):
+        existing_ids =[product.id for product in self.products]
+        if product_data["id"] in existing_ids:
+            print("Product with this ID already exists. Please choose another ID. ")
+        else:
+            new_product = Product(**product_data)
+            self.products.append(new_product)
+            self.database.save_data(self.products)
+            print("Product created succcessfully.")
 
     def read_products(self):
-        if not self.products:
-            self.product_views.display_message("No products available.")
-        else:
-            self.product_views.display_product(self.products)
-        
+        return self.products
 
-    def update_product(self):
-        product_id  = self.product_views.get_product_id()
+    def update_product(self, product_id, update_data):
         product = self.find_product_by_id(product_id)
-    
         if product:
-            update_data = self.product_views.get_update_products_data(product)
-            for key, value in update_data.items():
-                setattr(product,key, value) # cargamos los datos antes, para ver si soluciono el problema
-            self.database.save_data(self.products)
-            self.product_views.display_message("Product updated successfully.")
-        else:
-            self.product_views.display_message("Product not found.")
-    
+                for key, value in update_data.items():
+                    setattr(product, key, value)
 
-    def delete_product(self):
-        product_id = self.product_views.get_product_id()
+    def delete_product(self, product_id):
         product = self.find_product_by_id(product_id)
-
         if product:
-            self.products.remove(product) #acordate que estamos intenando hacer una persistencia
+            self.products.remove(product)
             self.database.save_data(self.products)
-            self.product_views.display_message("Product deleted successfully.")
+            print("Product deleted successfully.")
         else:
-            self.product_views.display_message("Product not found.")
-    
+            print("Product not found")
+
     def find_product_by_id(self, product_id):
         for product in self.products:
-            if str(product.id) == str(product_id):
+            if product.id == product_id:
                 return product
         return None
-
-    def run(self):
-        while True:
-            option = self.product_views.display_menu() # Aquí estamos haciendo un menú, un crud papito
-
-            if option == 1:
-                self.create_products() 
-            elif option == 2: 
-                self.read_products()
-            elif option == 3:
-                self.update_product()
-            elif option == 4:
-                self.delete_product()
-            elif option == 5:
-                self.product_views.display_menu("Exiting the program. Goodbye! see you later!") 
-                break
-            else:
-                self.product_views.display_message("Invalid option, Please Try again.")
-                
                 
