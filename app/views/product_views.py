8a6e94afffdd1df6b91e60db25ea267e923eb746
Ulises2156 <100093761+Ulises2156@users.noreply.github.com>
2024-02-13 17:23:38 -0300

    Update product_views.py

--- app/views/product_views.py
+++ app/views/product_views.py
@@ -1,28 +1,54 @@
 class ProductViews:
-    def display_menu(self, exit_message=None):
-        print("\n--- Menú Product ---")
+    def __init__(self, product_controller):
+        self.product_controller = product_controller
+
+    def run(self):
+        while True:
+            option = self.display_menu()
+
+            if option == 1:
+                product_data = self.get_product_data()
+                if product_data:
+                    self.product_controller.create_product(product_data)
+            elif option == 2:
+                products = self.product_controller.read_products()
+                self.display_products(products)
+            elif option == 3:
+                product_id = self.get_product_id()
+                update_data = self.get_update_data()
+                self.product_controller.update_product(product_id, update_data)
+                pass
+            elif option == 4:
+                product_id = self.get_product_id()
+                self.product_controller.delete_product(product_id)
+                pass
+            elif option == 5:
+                print("Exiting the program. Goodbye!")
+                break
+            else:
+                print("Invalid option. Please try again.")
+
+    def display_menu(self):
+        print("\n--- Menu ---")
         print("1. Create Product")
-        print("2. Read Product")
+        print("2. Read Products")
         print("3. Update Product")
         print("4. Delete Product")
         print("5. Exit")
-        if exit_message:
-            print(exit_message)
 
-        return int(input("Enter you choice: "))
+        return int(input("Enter your choice: "))
 
-    def get_products_data(self, existing_ids):
+    def get_product_data(self):
         while True:
             try:
                 product_id = int(input("Enter Product ID: "))
-                if product_id in existing_ids:
+                if self.product_controller.find_product_by_id(product_id):
                     print("Product with this ID already exits. Please choose another ID.")
                 else:
                     break
-            except ValueError: # estamos probrando un try y except, porque sigue saliendo error en id
+            except ValueError:
                 print("Invalid input. Please enter a valid numeric ID.")
-
-        name = input ("Enter Product Name: ")
+        name = input("Enter Product Name: ")
         description = input("Enter Product Description: ")
 
         while True:
@@ -30,45 +56,30 @@ class ProductViews:
                 price = float(input("Enter Product Price: "))
                 break
             except ValueError:
-                print("Invalid input. Please enter a valid numeric price")
-        while True:
-            try:
-                stock = int(input("Enter Product Stock: "))
-                break
-            except ValueError:
-                print("Invalid input. Please enter a valid numeric stock")
+                print("Invalid input. Please enter a valid numeric price.")
+        
+        stock = int(input("Enter Product Stock: "))
         category = input("Enter Product Category: ")
-        return {"product_id": product_id, 
-                "name": name, 
-                "description": description,
-                "price": price,
-                "stock": stock, 
-                "category": category }
+        return {"id": product_id, "name": name, "description": description, "price": price, "stock": stock, "category": category}
+    pass
 
-    def display_product(self, products):
-        print("\n---- Product List ----")
+    def display_products(self, products):
+        if not products:
+            print("No products available.")
+        else:
+            print("\n---- Product List ----")
         for product in products:
             print(product)
 
     def get_product_id(self):
         return int(input("Enter Product ID: "))
 
-    def get_update_products_data(self, product):
-        print("\n Enter new for the product (press Enter to kepp the current value):")
-        name = input(f"Current Name: {product.name}, Enter New Name: ") or product.name
-        description = input(f"Current Description: {product.description}, Enter New Description: ") or product.description
-        price = input(f"Current Price: {product.price}, Enter New Price: ") or product.price
-        stock = input(f"Current Stock: {product.stock}, Enter New Stock: ") or product. stock
-        category = input(f"Current Category: {product.category}, Enter New Category: ") or product.category
-        
-        update_data = {
-            "name": name,
-            "description": description,
-            "price": price,
-            "stock": stock,
-            "category": category
-        }
-        return update_data
-    
-    def display_message(self, message):
-        return message
+    def get_update_data(self):
+        print("\nEnter new data for the product (press Enter to keep the current value):")
+        name = input("Enter New Name: ")
+        description = input("Enter New Description: ")
+        price = input("Enter New Price: ")
+        stock = input("Enter New Stock: ")
+        category = input("Enter New Category: ")
+        return {"name": name, "description": description, "price": price, "stock": stock, "category": category}
+
