dd7cb4b472cf916d3d098982461cda0be83c2856
Ulises2156 <100093761+Ulises2156@users.noreply.github.com>
2024-02-27 19:50:33 -0300

    Update run.py

--- run.py
+++ run.py
@@ -2,9 +2,8 @@ from app.views.product_views import ProductViews
 from app.controllers.product_controller import ProductController
 from app.config.database import Database
 
-
 def main():
-    database = Database()
+    database= Database()
     product_controller = ProductController(database)
     product_views = ProductViews(product_controller)
     product_views.run()
