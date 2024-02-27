44916fb7ccf4bbaa12f130a9acf9cc495b07826a
Ulises2156 <100093761+Ulises2156@users.noreply.github.com>
2024-02-27 19:51:11 -0300

    Update database.py

--- app/config/database.py
+++ app/config/database.py
@@ -2,28 +2,28 @@ import csv
 from app.models.product import Product
 
 class Database:
-    def __init__(self, filename = "product_data.csv"):
+    def __init__(self, filename="product_data.csv"):
         self.filename = filename
     
     def save_data(self, products):
         try:
             with open(self.filename, "w", newline="") as csvfile:
-                fieldnames = ["id","name","description", "price", "stock", "category"]
-                writer = csv.DictWriter(csvfile, fieldnames= fieldnames)
+                fieldnames = ["id", "name", "description", "price", "stock", "category"]
+                writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
                 writer.writeheader()
                 for product in products:
                     writer.writerow({field: getattr(product, field) for field in fieldnames})
             print("Data saved successfully")
         except Exception as e:
-            print("Erro saving data:", e)
+            print("Error saving data:", e)
 
     def load_data(self):
         data = []
         try:
-            with open(self.filename, "r", newline= "") as csvfile:
+            with open(self.filename, "r", newline="") as csvfile:
                 reader = csv.DictReader(csvfile)
                 for row in reader:
-                    product_data ={
+                    product_data = {
                         "id": row["id"],
                         "name": row["name"],
                         "description": row["description"],
@@ -31,12 +31,45 @@ class Database:
                         "stock": row["stock"],
                         "category": row["category"],
                     }
-                    product =Product(**product_data)
+                    product = Product(**product_data)
                     data.append(product)
             print("Data loaded successfully.")
         except FileNotFoundError:
             print("File not found.")
         except Exception as e:
-            print("Erro loading data:", e)
+            print("Error loading data:", e)
         return data
-        
+    
+    def load_all(filename="product:data.csv"):
+        products = []
+        try:
+            with open(filename, "r", newline="") as csvfile:
+                reader = csv.DictReader(csvfile)
+                for row in reader:
+                    product = Product(
+                        row["id"],
+                        row["name"],
+                        row["description"],
+                        row["price"],
+                        row["stock"],
+                        row["category"]
+                    )
+                    products.append(product)
+        except FileNotFoundError:
+            pass
+        return products
+    
+    def save(self, products, filename="product_data.csv"):
+        with open(filename, "w", newline="") as csvfile:
+            fieldnames = ["id", "name", "description", "price", "stock", "category"]
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            writer.writeheader()
+            for product in products:
+                writer.writerow({field: getattr(product, field) for field in fieldnames})
+            print("Data saved successfully")
+    
+    def delete_product(self, product_id):
+        products = self.load_data()
+        updated_products = [product for product in products if product.id != product_id]
+        self.save_data(updated_products)
+        print("Product deleted successfully.")
